a7ec5cdc2f6b5265812db35c0c66d3e2338df6c1
Luca Casonato <lucacasonato@yahoo.com>
2020-07-11 17:28:36 +0200

    Update

--- mod.ts
+++ mod.ts
@@ -1,13 +1,13 @@
-import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/util.ts";
+import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/util.ts";
 
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/client.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/collection.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/database.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/result.ts";
-export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/types.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/v0.8.0/ts/util.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/client.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/collection.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/database.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/result.ts";
+export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/types.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/2503375425009ce2cec694a0d829d9d2d1444a8d/ts/util.ts";
 
-export const VERSION = "v0.8.0";
+export const VERSION = "2503375425009ce2cec694a0d829d9d2d1444a8d";
 export const RELEASE_URL = `https://github.com/lucacasonato/deno_mongo_lambda/releases/download/${VERSION}`;
 
 await init(RELEASE_URL);
