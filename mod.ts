ce758a139f05b43a020659af79de545a3544f893
Luca Casonato <lucacasonato@yahoo.com>
2020-07-14 19:31:02 +0200

    Update deno_mongo

--- mod.ts
+++ mod.ts
@@ -1,11 +1,11 @@
-import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/util.ts";
+import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/util.ts";
 
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/client.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/collection.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/database.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/result.ts";
-export type { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/types.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/a3d2bbfca359efbc2941d16758805e3520d9ed89/ts/util.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/client.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/collection.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/database.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/result.ts";
+export type { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/types.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/util.ts";
 
 export const VERSION = "v0.9.0-dev2";
 export const RELEASE_URL =
