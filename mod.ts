3f407a302c883f1a84fc6c4aebf28a45ff3aa49e
Luca Casonato <lucacasonato@yahoo.com>
2020-07-17 00:33:02 +0200

    Fix ObjectId export

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b7519
 export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/collection.ts";
 export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/database.ts";
 export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/result.ts";
-export type { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/types.ts";
+export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/types.ts";
 export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/util.ts";
 
 export const VERSION = "v0.9.0-dev2";
