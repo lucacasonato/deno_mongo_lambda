8a76be3d1996a6abf9d2cf1e999c741314312dcb
Luca Casonato <lucacasonato@yahoo.com>
2020-08-07 13:06:54 +0200

    Update to v0.10.0

--- mod.ts
+++ mod.ts
@@ -1,13 +1,13 @@
-import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/util.ts";
+import { init } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/util.ts";
 
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/client.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/collection.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/database.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/result.ts";
-export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/types.ts";
-export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/6f1b75193a221ac03b87904313645c389d3c89e7/ts/util.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/client.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/collection.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/database.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/result.ts";
+export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/types.ts";
+export * from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/3f5b89057efe28f08beec4fcb6206ec15833de0e/ts/util.ts";
 
-export const VERSION = "v0.9.0-dev2";
+export const VERSION = "v0.10.0";
 export const RELEASE_URL =
   `https://github.com/lucacasonato/deno_mongo_lambda/releases/download/${VERSION}`;
 
